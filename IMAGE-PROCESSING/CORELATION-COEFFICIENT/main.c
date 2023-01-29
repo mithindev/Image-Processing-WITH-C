4faba115841d1b33e9fb4182bc0bd381fa9bc626
mithindev <mithindev1@gmail.com>
2023-01-29 20:51:25 +0530

    updated

--- IMAGE-PROCESSING/CORELATION-COEFFICIENT/main.c
+++ IMAGE-PROCESSING/CORELATION-COEFFICIENT/main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+double CorrelationCoefficient(int *image1, int *image2, int size);
 
 int main() {
        // Open the input images
     
     FILE *f1 = fopen("test1p6.ppm", "rb");
     
-    FILE *f2 = fopen("test1p6.ppm", "rb");
+    FILE *f2 = fopen("test1p6c.ppm", "rb");
     
     if (!f1 || !f2) {
         printf("Error: Failed to open one of the input images\n");
@@ -17,6 +20,7 @@ int main() {
     char header[3];
     int width, height, maxValue;
     fscanf(f1, "%s %d %d %d", header, &width, &height, &maxValue);
+    printf(f1, "%s %d %d %d", header, &width, &height, &maxValue);
 
     // Make sure the image is in P6 PPM format
     if (header[0] != 'P' || header[1] != '6') {
@@ -26,6 +30,7 @@ int main() {
 
     // Read the header of the second image
     fscanf(f2, "%s %d %d %d", header, &width, &height, &maxValue);
+    printf(f2, "%s %d %d %d", header, &width, &height, &maxValue);
 
     // Make sure the image is in P6 PPM format
     if (header[0] != 'P' || header[1] != '6') {
@@ -43,6 +48,10 @@ int main() {
 
     // Do something with the pixel data, for example, compare them
 
+    int size = sizeof(pixels1) / sizeof(pixels1[0]); 
+    double similarity = correlationCoefficient(pixels1, pixels2, size);
+    printf("Similarity: %f", similarity);
+
     // Free the memory
     free(pixels1);
     free(pixels2);
@@ -52,4 +61,23 @@ int main() {
     fclose(f2);
 
     return 0;
+}
+
+double CorrelationCoefficient(int *img1, int *img2, int size) {
+    double mean1 = 0, mean2 = 0;
+    for (int i = 0; i < size; i++) {
+        mean1 += img1[i];
+        mean2 += img2[i];
+    }
+    mean1 /= size;
+    mean2 /= size;
+
+    double numerator = 0, denominator1 = 0, denominator2 = 0;
+    for (int i = 0; i < size; i++) {
+        numerator += (img1[i] - mean1) * (img2[i] - mean2);
+        denominator1 += pow((img1[i] - mean1), 2);
+        denominator2 += pow((img2[i] - mean2), 2);
+    }
+
+    return numerator / sqrt(denominator1 * denominator2);
 }
